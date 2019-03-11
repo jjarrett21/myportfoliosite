48f62c1ac6e827e087f6e38fd04fffe4f90b87e0
James Jarrett <jjarrett21@gmail.com>
2019-03-10 19:08:23 -0500

    Updates

---
+++ precache-manifest.69a41ec5c4556458ca869ff6fc5edecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aafc2f6e0dedb34d1b2e",
+    "url": "/myportfoliosite/home/static/css/main.50e5d58d.chunk.css"
+  },
+  {
+    "revision": "aafc2f6e0dedb34d1b2e",
+    "url": "/myportfoliosite/home/static/js/main.bd100fa4.chunk.js"
+  },
+  {
+    "revision": "856c50e4b4e50aa37340",
+    "url": "/myportfoliosite/home/static/js/runtime~main.e3a6d3ab.js"
+  },
+  {
+    "revision": "e853fed8b5a8f4d630bb",
+    "url": "/myportfoliosite/home/static/css/2.ad89ae36.chunk.css"
+  },
+  {
+    "revision": "e853fed8b5a8f4d630bb",
+    "url": "/myportfoliosite/home/static/js/2.f3dfbb07.chunk.js"
+  },
+  {
+    "revision": "e407bd3ebb051ff96989ef995d6a4ae3",
+    "url": "/myportfoliosite/home/static/media/James-avi.e407bd3e.png"
+  },
+  {
+    "revision": "f38fd61d2c26796c6f0c44de3186cb33",
+    "url": "/myportfoliosite/home/index.html"
+  }
+];
