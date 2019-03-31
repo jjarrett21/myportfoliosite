0efc712f6e1929ab0fd0d0bd36e288d8c45decb5
James Jarrett <jjarrett21@gmail.com>
2019-03-31 16:52:18 -0500

    Reverted to another commit to fix the werid deletions that happened when deploying to gh-pages, installed antd over material ui, converting things to stateless components

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,50 +1,48 @@
-
-
-import React from 'react'
+import React, { Component } from 'react'
+// import { Grid, Cell } from 'react-mdl';
 import avatar from '../Images/James-avi.png'
 import { Layout, Row, Col } from 'antd'
 
-function LandingPage() {
-    return (
-        <div>
-            <Row type="flex" align="middle" justify="center">
-                <Col span={12} className="landing-grid">
-                    <img
-                        src={avatar}
+class LandingPage extends Component {
+    render() {
+        return(
+            <div style={{width: '100%', margin: 'auto'}}> 
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img 
+                        src= {avatar}
                         alt="avatar"
                         className="avatar-img"
-                    />
+                        />
 
-
-                    <div className="banner-text">
+                    <div className="banner-text"> 
                         <h1>Software and Electrical Engineer</h1>
 
-                        <hr />
+                        <hr/>
                         <p>IOS Development | React | Java | JavaScript | SQL | Python | VHDL | C </p>
 
                         <div className="social-links">
                             {/* LinkedIn */}
                             <a href="https://www.linkedin.com/in/jjarrett21/" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-linkedin-square" aria-hidden="true" />
+                            <i className="fa fa-linkedin-square"  aria-hidden="true" />
                             </a>
 
                             {/* Github */}
                             <a href="https://github.com/jjarrett21" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-github-square" aria-hidden="true" />
+                            <i className="fa fa-github-square"  aria-hidden="true" />
                             </a>
 
                             {/* Twitter */}
                             <a href="https://twitter.com/__youngcreator" rel="noopener noreferrer" target="_blank">
-                                <i className="fa fa-twitter-square" aria-hidden="true" />
+                            <i className="fa fa-twitter-square"  aria-hidden="true" />
                             </a>
                         </div>
                     </div>
-
-                </Col>
-            </Row>
-        </div>
-    )
-
+                    </Cell>
+                </Grid>
+            </div>
+        )
+    }
 }
 
-export default LandingPage
+export default LandingPage;
